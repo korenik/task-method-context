6d144b2bfdf75e800728476a488b3da85cca0eff
korenik <vlad-r-87@rambler.ru>
2022-04-03 13:35:35 +0300

    HW task-method-context

--- src/MethodContext.js
+++ src/MethodContext.js
@@ -8,8 +8,13 @@ import { EventEmitter } from './EventEmitter';
  */
 export const obj = {
     count: 0,
-    subscribe() {},
-    unsubscribe() {},
+    counter() { this.count++ },
+    subscribe() {
+       EventEmitter.on('click', this.counter)
+    },
+    unsubscribe() {
+       EventEmitter.off('click', this.counter) 
+    },
 };
 
 /*
@@ -19,8 +24,10 @@ obj1.first(1, 2, 3);
 // Внутренний вызов должен быть равносилен obj1.second(3, 2, 1)
  */
 export const obj1 = {
-    first(...args) {},
-    second() {
-        // здесь ничего писать не нужно
-    },
+    first(...args) {
+        obj1.second.reverse(); 
+     },
+     second() {
+         // здесь ничего писать не нужно
+     },
 };
